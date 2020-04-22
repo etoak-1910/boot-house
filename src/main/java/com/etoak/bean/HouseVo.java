f2587ef0f10ae7e6488ce6c5c61a885f8ceb15cd
lihongyue <271094994@qq.com>
2020-04-22 17:20:29 +0800

    提交HouseVo>java和 Page.java

---
+++ src/main/java/com/etoak/bean/HouseVo.java
@@ -0,0 +1,14 @@
+package com.etoak.bean;
+
+
+import lombok.Data;
+
+@Data
+public class HouseVo {
+    private String rentModeName;
+    private String houseTypeName;
+    private String orientationName;
+
+
+
+}
