7bb0db9129fda143320e569a79b969e8d8aaa5e9
lihongyue <271094994@qq.com>
2020-04-22 17:16:46 +0800

    房源查询接口完成一部分

--- src/main/java/com/etoak/mapper/HouseMapper.java
+++ src/main/java/com/etoak/mapper/HouseMapper.java
@@ -1,6 +1,9 @@
 package com.etoak.mapper;
 
 import com.etoak.bean.House;
+import com.etoak.bean.HouseVo;
+
+import java.util.List;
 
 public interface HouseMapper {
     /**
@@ -9,5 +12,7 @@ public interface HouseMapper {
      * @return
      */
     int addHouse(House house);
+
+    List<HouseVo> queryList(HouseVo houseVo);
 
 }
